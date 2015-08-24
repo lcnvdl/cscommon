dc7ca0a46f94c97d345ba427be017c5337ee3bfc
Lcnvdl <programando.soft@gmail.com>
2015-08-24 16:08:43 -0300

    Agregada carpeta IO, y agregado Embedded
    Agregada carpeta IO, y agregado Embedded. Embedded permite fácilmente
    copiar recursos embebidos a archivos.

---
+++ Common - 45/Common/IO/Embedded.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Common.IO
+{
+    public static class Embedded
+    {
+        private static void CopyEmbeddedResources(this Assembly assembly, string outputDir, string resourceLocation, params string[] files)
+        {
+            foreach (string file in files)
+            {
+                using (Stream stream = assembly.GetManifestResourceStream(resourceLocation + @"." + file))
+                {
+                    using (FileStream fileStream = new FileStream(Path.Combine(outputDir, file), FileMode.Create))
+                    {
+                        for (int i = 0; i < stream.Length; i++)
+                        {
+                            fileStream.WriteByte((byte)stream.ReadByte());
+                        }
+                        fileStream.Close();
+                    }
+                }
+            }
+        }
+    }
+}
