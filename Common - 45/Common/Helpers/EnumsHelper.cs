775c464ec605b30ac4876a29b6e8521fc5bb4962
Lcnvdl <programando.soft@gmail.com>
2018-03-14 20:22:36 -0300

    Enums helper agregado
    Enums helper agregado

---
+++ Common - 45/Common/Helpers/EnumsHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.Helpers
+{
+    public static class EnumsHelper
+    {
+        public static IEnumerable<T> Split<T>(T flags)
+        {
+            int iFlags = (int)((object)flags);
+            return Enum.GetValues(typeof(T)).Cast<T>().Where(s => (iFlags & ((int)(object)s)) == (int)((object)s));
+        }
+    }
+}
